66fd76111eda0860ec1f28280b7d39e9db49591f
Nicholas Tierney <nicholas.tierney@gmail.com>
2019-01-30 11:32:37 -0800

    add re-exports

---
+++ R/usethis-port.R
@@ -0,0 +1,19 @@
+#' @importFrom usethis use_readme_rmd
+#' @export
+usethis::use_readme_rmd()
+
+#' @importFrom usethis use_readme_md
+#' @export
+usethis::use_readme_md()
+
+#' @importFrom usethis use_readme_md
+#' @export
+usethis::use_readme_md()
+
+#' @importFrom usethis use_cc0_license
+#' @export
+usethis::use_cc0_license()
+
+#' @importFrom usethis use_citation
+#' @export
+usethis::use_citation()
